a9dfafa5fa9b0ddea450a3978f4b1b31bb878da4
Yulia Trysniuk <juliatrysnjuk@gmail.com>
2025-06-21 17:18:32 -0500

    Практична робота 2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Створюємо контекст даних
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // Отримуємо з бази даних всі об’єкти Book
+            IEnumerable<Book> books = db.Books;
+            // передаємо всі отримані об’єкти в динамічну властивість Books в ViewBag
+            ViewBag.Books = books;
+            // повертаємо представлення
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // Додаємо інформацію про купівлю в базу даних
+            db.Purchases.Add(purchase);
+            // Зберігаємо в базу даних всі зміни
+            db.SaveChanges();
+            return "Дякуємо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
---
+++ Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Кобзар", Author = "Т.Г. Шевченко", Price = 220 });
+            db.Books.Add(new Book { Name = "Зона покриття", Author = "Стівен Кінг", Price = 180 });
+            db.Books.Add(new Book { Name = "Сяйво", Author = "Стівен Кінг", Price = 200 });
+            db.Books.Add(new Book { Name = "Захар Беркут", Author = "Іван Франко", Price = 160 });
+            base.Seed(db);
+        }
+    }
+}
