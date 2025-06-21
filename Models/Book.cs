42fb1eaae5680c31c6fbc0220f0eecdd296f5940
Yulia Trysniuk <juliatrysnjuk@gmail.com>
2025-06-21 14:51:58 -0500

    Initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        // ID книги
+        public int Id { get; set; }
+        // Назва книги
+        public string Name { get; set; }
+        // Автор книги
+        public string Author { get; set; }
+        // Ціна
+        public int Price { get; set; }
+    }
+}
---
+++ Models/Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        // ім’я та прізвище покупця
+        public string Person { get; set; }
+        // адреса покупця
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        // дата покупки
+        public DateTime Date { get; set; }
+
+        // public virtual Book Book { get; set; }
+    }
+}
